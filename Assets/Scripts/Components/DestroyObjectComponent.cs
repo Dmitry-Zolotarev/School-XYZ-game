c240b64b9ad4d72e5ef003d776f0e099cfbefd5c
Dmitry-Zolotarev <dimazed04@gmail.com>
2025-12-01 20:00:10 +0700

    Добавлена разрушаемая бочка

--- Assets/Scripts/Components/DestroyObjectComponent.cs
+++ Assets/Scripts/Components/DestroyObjectComponent.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class DestroyObjectComponent : MonoBehaviour
 {
     [SerializeField] private float latency = 0.1f;
+    [SerializeField] private bool destroyAtSpawn = false;
+    private void Start()
+    {
+        if (destroyAtSpawn) DestroyObject();
+    }
     public void DestroyObject()
     {
         Destroy(gameObject, latency);
